20e52b8f0511dc51db1268432194a98619ff6231
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-08-27 22:47:17 +0800

    Update cache version to v133

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kirin-komikku-v132';
+const CACHE = 'kirin-komikku-v133';
 const SHELL = [
   './', './index.html', './assets/css/app.css', './assets/js/app.js', './assets/vendor/pako.min.js',
   './schemas/schema-komikku.proto', './manifest.webmanifest', './assets/icons/app-icon.svg',
